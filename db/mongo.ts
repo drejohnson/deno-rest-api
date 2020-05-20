785132658b77104a953e0bacaf008fc4e0c6270d
DeAndre Johnson <dre@phreshr.com>
2020-05-21 01:50:47 +0800

    add mongodb

---
+++ db/mongo.ts
@@ -0,0 +1,12 @@
+import { MongoClient } from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+import { config } from "https://deno.land/x/dotenv/mod.ts";
+
+config({ safe: true, export: true });
+
+// i83a5ThEaloT8g9E
+const client = new MongoClient();
+client.connectWithUri(String(Deno.env.get("MONGO_URI")));
+
+const db = client.database("test");
+
+export default db;
