a09fbdf673baa08f82bb4943f1be22f89e8833c5
DeAndre Johnson <dre@phreshr.com>
2020-05-22 08:30:11 +0800

    add type for route param

--- routes.ts
+++ routes.ts
@@ -11,9 +11,9 @@ const router = new Router();
 
 router
   .get("/api/v1/products", getProducts)
-  .get("/api/v1/products/:id", getProduct)
+  .get<{ id: string }>("/api/v1/products/:id", getProduct)
   .post("/api/v1/products", addProduct)
-  .put("/api/v1/products/:id", updateProduct)
-  .delete("/api/v1/products/:id", deleteProduct);
+  .put<{ id: string }>("/api/v1/products/:id", updateProduct)
+  .delete<{ id: string }>("/api/v1/products/:id", deleteProduct);
 
 export default router;
